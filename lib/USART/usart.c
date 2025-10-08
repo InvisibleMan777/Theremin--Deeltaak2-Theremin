b54560ff08603652f7b593bab791af25724dd1c8
Sem Overbeek <s1212918@student.windesheim.nl>
2025-10-08 20:34:12 +0200

    init

---
+++ lib/USART/usart.c
@@ -0,0 +1,50 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define BAUD 9600
+#include <util/setbaud.h>	// for UBBRL_VALUE etc.
+#include "usart.h"
+
+void USART_Init(void) {
+	UBRR0H = UBRRH_VALUE;
+	UBRR0L = UBRRL_VALUE;
+
+	// default: 8 data bits; 1 stop bit
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
+
+	// enable
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+}
+
+void USART_Init_RXinterrupt(void) {
+	// enable RX interrupts
+	UCSR0B |= (1 << RXCIE0);
+
+	// enable global interrupts
+	sei();
+}
+
+void USART_Transmit_Char(uint8_t b) {
+	// Wait for empty transmit buffer
+	while ( !( UCSR0A & (1<<UDRE0)) );
+
+	// Put data into transmit buffer, sends the data
+	UDR0 = b;
+}
+
+uint8_t USART_Receive(void) {
+	// Wait for receive complete
+	while ( !( UCSR0A & (1<<RXC0)) );
+
+	// Return data from receive buffer buffer
+	return(UDR0);
+}
+
+void USART_Transmit_Line(char* str) {
+    // Transmit every character in the string until the nullterminator (equal to false) is reached
+    while (*str) {
+        USART_Transmit_Char(*str++);
+    }
+    USART_Transmit_Char('\n');
+}
+
---
+++ lib/USART/usart.h
@@ -0,0 +1,19 @@
+#ifndef USART_H
+#define USART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* prototypes */
+void USART_Init();
+void USART_Init_RXinterrupt();
+void USART_Transmit_Char(uint8_t b);
+void USART_Transmit_Line(char* str);
+uint8_t USART_Receive(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
