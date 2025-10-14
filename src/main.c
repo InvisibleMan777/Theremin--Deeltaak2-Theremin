29e7c2f19648c966ab8bab31f8dbe23993bed702
Sem Overbeek <s1212918@student.windesheim.nl>
2025-10-14 22:13:00 +0200

    restructuring main for better readability

--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
+//standard libraries
 #include <avr/io.h>
-#include <stdbool.h>
+#include <avr/interrupt.h>
 #include <stdio.h>
 #include <math.h> 
-#include <inttypes.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 
+//internal libraries
 #include "usart.h"
 #include "timetracking.h"
 #include "medianCalculator.h"
 
+//constants
 #define MAX_SAMPLES 10 // number of samples to take for median filtering
 #define MAX_DISTANCE_MM 650 // maximum distance measurable by the sensor in mm
 #define MIN_DISTANCE_MM 20 // minimum distance measurable by the sensor in mm
@@ -20,13 +21,15 @@
 volatile uint32_t echoTimeStart; // time when echo is received
 volatile uint8_t sampleIndex = 0; // current index in samples array
 
-uint32_t timeDiffSamples[MAX_SAMPLES] = {0}; // array to hold last 10 samples of distances
+//global variables
+uint32_t timeDiffSamples[MAX_SAMPLES] = {0}; // array to hold last MAX samples of distances
 uint32_t TimeSinceLastTrigger; // time since last trigger of sonar sensor
 uint32_t usartPrintStartTime; // time since last USART print
-uint32_t medianTimeDiff = 0; // median of last 10 samples
+uint32_t medianTimeDiff = 0; // median of last MAX samples
 uint32_t distance = 0; // distance in cm
 uint16_t FREQ_BUZZER = 440; //frequency of buzzer in Hz
 
+//buffers
 char timer0CompareValueA; //buffer for OCR0A to set buzzer frequency
 char message[255] = ""; //message buffer used to transmit distance over usart
 
@@ -38,76 +41,76 @@ ISR(PCINT2_vect) {
 
     //stop timing on falling edge, save time difference in samples array
     } else if (!(PIND & (1 << PIND5))) {
-            //replace oldest sample with new sample on falling edge
+            //replace oldest sample with new sample
             timeDiffSamples[sampleIndex] = micros() - echoTimeStart;
-            //wrap around last index
+            //next oldest sample is one index higher, wrap around at last index using modulo (max+1 % max = 0)
             sampleIndex = (sampleIndex + 1) % MAX_SAMPLES;
-        }
     }
+}
 
 //timer0 compare interrupt service routine for buzzer
 ISR(TIMER0_COMPA_vect) {
-    // toggle PD3
+    //toggle PD3
     PORTD ^= (1 << PORTD3);
 }
 
 //initialize regestries for sonar sensor
 void initSonarSensor() {
-    // initalize trigger pin (PD4) as output
-    DDRD |= (1 << DDD4);
+    DDRD |= (1 << DDD4); //initalize trigger pin (PD4) as output
 
-    // initialize interrupt on echo pin (PD5)
+    //enable interrupt
     PCICR |= (1 << PCIE2); // enable pin change interrupt for PORTD
     PCMSK2 |= (1 << PCINT21); // enable interrupt for PIND5
-    return;
 }
 
 //initialize regestries for buzzer
 void initBuzzer() {
-    // set PD3 as output for buzzer
-    DDRD |= (1 << DDD3);
-    return;
-}
+    DDRD |= (1 << DDD3); // set PD3 (connected to buzzer) as output
 
-void initTimer0() {
+    //init timer0
     TCCR0A = (1 << WGM01); // set CTC mode
     TCCR0B = (1 << CS02); // set prescaler to 256
     TIMSK0 |= (1 << OCIE0A); // enable timer compare interrupt for match A
-    return;
 }
 
 int main() {
+    //initialize usart communication for debugging
     USART_Init();
     USART_Transmit_Line("Hello, USART!");
+    //initialize time tracking so we can use millis() and micros()
+    timerTrackingInit();
+    //initialize sonar sensor and buzzer
     initSonarSensor();
     initBuzzer();
-    initTimer0();
-    timerTrackingInit();
-    sei(); // enable global interrupts
+    //enable global interrupts
+    sei(); 
 
-    // main loop
+    //main loop
     for(;;) {
-        //trigger trigger pin every 100ms
+        //trigger sonar every x ms
         if (millis() - TimeSinceLastTrigger > 1) {
+            //enable trigger pin for 10 microseconds
             PORTD |= (1 << PORTD4);
-            _delay_us(10); // 10 microsecond pulse
+            _delay_us(10);
             PORTD &= ~(1 << PORTD4);
+            //reset timer
             TimeSinceLastTrigger = millis();
         }
 
         // calculate median of last 10 samples
         medianTimeDiff = calculateMedian_uint32(timeDiffSamples, MAX_SAMPLES);
 
+        //calculate distance in mm: distance = (timeDiff * speed of sound) / 2
         distance = round((medianTimeDiff * 0.343) / 2);
 
-        //set distance to MIN if smaller then MIN, buzzer stops buzzing when distance is greater than max
+        //cap distance to MAX_DISTANCE_MM and MIN_DISTANCE_MM
         if (distance < MIN_DISTANCE_MM) {
             distance = MIN_DISTANCE_MM;
         } else if (distance > MAX_DISTANCE_MM) {
             distance = MAX_DISTANCE_MM;
         }
 
-        //mapping distance to frequency linearly: frequenty = ((dmax - distance + dmin) / (dmax - dmin)) * (fmax - fmin) + fmin
+        //mapping distance to frequency negatively linearly: frequenty = ((dmax - distance + dmin) / (dmax - dmin)) * (fmax - fmin) + fmin
         //casting to double to prevent integer division (which would result in 0 for distances < dmax)
         FREQ_BUZZER = round(((MAX_DISTANCE_MM - distance + MIN_DISTANCE_MM) / (double)(MAX_DISTANCE_MM - MIN_DISTANCE_MM)) * (MAX_FREQ_HZ - MIN_FREQ_HZ) + MIN_FREQ_HZ);
 
@@ -115,7 +118,7 @@ int main() {
         timer0CompareValueA = round(31250 / FREQ_BUZZER) - 1;
         OCR0A = timer0CompareValueA;
 
-        // print distance every 500ms
+        // print distance every x ms
         if (millis() - usartPrintStartTime > 100) {
             //load distance into message buffer
             sprintf(message, "distance: %lu", distance);
